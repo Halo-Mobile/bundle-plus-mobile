a9d08a7d563339817ca52adb90f7d22f342d7964
adamatnoh <76959023+adamatnoh@users.noreply.github.com>
2022-05-04 03:38:17 +0800

    Fix

--- bundle_plus/lib/screens/sign_up/components/sign_up_form.dart
+++ bundle_plus/lib/screens/sign_up/components/sign_up_form.dart
@@ -3,10 +3,10 @@ import 'package:shop_app/components/custom_surfix_icon.dart';
 import 'package:shop_app/components/default_button.dart';
 import 'package:shop_app/components/form_error.dart';
 import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../constants.dart';
 import '../../../size_config.dart';
-
 
 class SignUpForm extends StatefulWidget {
   @override
