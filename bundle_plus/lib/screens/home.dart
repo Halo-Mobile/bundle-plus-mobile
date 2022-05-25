c131f012e7d9ccdccdb117dc8c55bca37cee6d9a
adamatnoh <76959023+adamatnoh@users.noreply.github.com>
2022-05-26 01:41:34 +0800

    homepage

--- bundle_plus/lib/screens/home.dart
+++ bundle_plus/lib/screens/home.dart
@@ -14,6 +14,56 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildFeaturedProduct({String? name, double? price, String? image}) {
+    String price2 = price!.toStringAsFixed(2);
+    return Card(
+      child: Container(
+        height: 250,
+        width: 180,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 190,
+              width: 160,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/$image"),
+                ),
+              ),
+            ),
+            Text(
+              "RM $price2",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black),
+            ),
+            Text(
+              name!,
+              style: TextStyle(
+                  // fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryProduct(String icon) {
+    int icon2 = int.parse(icon);
+    return CircleAvatar(
+      maxRadius: 43,
+      backgroundColor: Colors.pinkAccent,
+      child: IconButton(
+          icon: Icon(IconData(icon2, fontFamily: 'MaterialIcons'),
+              color: Colors.white),
+          onPressed: () {}),
+    );
+  }
+
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
 
@@ -33,67 +83,256 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
+      endDrawerEnableOpenDragGesture: false,
+      drawer: Drawer(),
       appBar: AppBar(
-        title: const Text("Welcome"),
+        title: const Text(
+          "Bundle+",
+          style:
+              TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 150,
-                child: Image.asset("assets/logo.png", fit: BoxFit.contain),
-              ),
-              Text(
-                "Welcome Back",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text("${loggedInUser.firstName} ${loggedInUser.secondName}",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontWeight: FontWeight.w500,
-                  )),
-              Text("${loggedInUser.email}",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontWeight: FontWeight.w500,
-                  )),
-              SizedBox(
-                height: 15,
-              ),
-              ActionChip(
-                  label: Text("Logout"),
-                  onPressed: () {
-                    logout(context);
-                  }),
-                  Material(
-                    elevation: 5,
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.pinkAccent,
-                    child: MaterialButton(
-                      padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                      minWidth: 200.0,
-                      onPressed: () {
-                        Navigator.push(
-                            context, MaterialPageRoute(builder: (context) => const ProfilePage()));
-                      },
-                      child: const Text(
-                        "Edit Profile",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  )
-            ],
+        elevation: 0.0,
+        backgroundColor: Colors.grey[100],
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.pinkAccent,
           ),
+          onPressed: () => _key.currentState?.openDrawer(),
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.notifications_none, color: Colors.pinkAccent),
+              onPressed: () {}),
+          IconButton(
+              icon:
+                  Icon(Icons.account_circle_outlined, color: Colors.pinkAccent),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfilePage()));
+              }),
+        ],
+      ),
+      // body: Center(
+      //   child: Padding(
+      //     padding: EdgeInsets.all(20),
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       crossAxisAlignment: CrossAxisAlignment.center,
+      //       children: <Widget>[
+      //         SizedBox(
+      //           height: 150,
+      //           child: Image.asset("assets/logo.png", fit: BoxFit.contain),
+      //         ),
+      //         Text(
+      //           "Welcome Back",
+      //           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Text("${loggedInUser.firstName} ${loggedInUser.secondName}",
+      //             style: TextStyle(
+      //               color: Colors.black54,
+      //               fontWeight: FontWeight.w500,
+      //             )),
+      //         Text("${loggedInUser.email}",
+      //             style: TextStyle(
+      //               color: Colors.black54,
+      //               fontWeight: FontWeight.w500,
+      //             )),
+      //         SizedBox(
+      //           height: 15,
+      //         ),
+      //         ActionChip(
+      //             label: Text("Logout"),
+      //             onPressed: () {
+      //               logout(context);
+      //             }),
+      //             Material(
+      //               elevation: 5,
+      //               borderRadius: BorderRadius.circular(30),
+      //               color: Colors.pinkAccent,
+      //               child: MaterialButton(
+      //                 padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+      //                 minWidth: 200.0,
+      //                 onPressed: () {
+      //                   Navigator.push(
+      //                       context, MaterialPageRoute(builder: (context) => const ProfilePage()));
+      //                 },
+      //                 child: const Text(
+      //                   "Edit Profile",
+      //                   textAlign: TextAlign.center,
+      //                   style: TextStyle(
+      //                       fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+      //                 ),
+      //               ),
+      //             )
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 120,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          hintText: "Seach",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Featured",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "See all",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            _buildFeaturedProduct(
+                                image: "acer.jfif",
+                                price: 3500,
+                                name: "ACER Predator Helios 300"),
+                            _buildFeaturedProduct(
+                                image: "ipx.jpg",
+                                price: 1500,
+                                name: "iPhone X"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Categories",
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "See all",
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      _buildCategoryProduct("0xe3dd"),
+                      _buildCategoryProduct("0xe367"),
+                      _buildCategoryProduct("0xe25a"),
+                      _buildCategoryProduct("0xe5f3"),
+                      _buildCategoryProduct("0xe5ef"),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "New Arrival",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "See all",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              _buildFeaturedProduct(
+                                  image: "dilo.jpg",
+                                  price: 15,
+                                  name: "Digital Logic"),
+                              _buildFeaturedProduct(
+                                  image: "oop.jpg",
+                                  price: 20.5,
+                                  name: "OOP using JAVA"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -108,12 +347,11 @@ class _HomeScreenState extends State<HomeScreen> {
   //     }
   //   });
   // }
-  
 
   // the logout function
-  Future<void> logout(BuildContext context) async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoginScreen()));
-  }
+  // Future<void> logout(BuildContext context) async {
+  //   await FirebaseAuth.instance.signOut();
+  //   Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(builder: (context) => LoginScreen()));
+  // }
 }
--- bundle_plus/lib/screens/profilepage.dart
+++ bundle_plus/lib/screens/profilepage.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../model/user_model.dart';
 import '../model/user_profile.dart';
+import 'login.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -13,47 +14,43 @@ class ProfilePage extends StatefulWidget {
   _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage>{
-  Widget _buildSingleContainer({String startText="", String endText=""}){
+class _ProfilePageState extends State<ProfilePage> {
+  Widget _buildSingleContainer({String startText = "", String endText = ""}) {
     return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Container(
+        height: 50,
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              startText,
+              style: TextStyle(fontSize: 18, color: Colors.black45),
+            ),
+            Text(
+              endText,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
-        child: Container(
-          height: 50,
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          decoration: BoxDecoration(
-            color:  Colors.white,
-            borderRadius: BorderRadius.circular(30)
-          ),
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                startText,
-                style: TextStyle(fontSize: 18,color: Colors.black45), 
-                ),
-                Text(
-                endText,
-                style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold), 
-                ),
-            ],
-          ),
-        ),
-      );
-  }
-  Widget _buildSingleTextFormField({String name=""}){
-    return TextFormField(
-      decoration: InputDecoration(
-        hintText: name,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
-        )
       ),
     );
   }
-  
+
+  Widget _buildSingleTextFormField({String name = ""}) {
+    return TextFormField(
+      decoration: InputDecoration(
+          hintText: name,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          )),
+    );
+  }
+
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
   UserProfile userprofile = UserProfile();
@@ -106,8 +103,8 @@ class _ProfilePageState extends State<ProfilePage>{
         padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: 200.0,
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const ProfilePageEdit()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ProfilePageEdit()));
         },
         child: const Text(
           "Edit Profile",
@@ -118,86 +115,101 @@ class _ProfilePageState extends State<ProfilePage>{
       ),
     );
 
+    var children2 = [
+      Stack(
+        children: [
+          Container(
+            height: 140,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // Image.asset("assets/logo.png", fit: BoxFit.contain),
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  maxRadius: 70,
+                  backgroundImage: AssetImage("assets/eleceed.jpg"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      Container(
+        height: 300,
+        // color: Colors.blue,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildSingleContainer(
+                    endText: "${loggedInUser.firstName}",
+                    startText: "First Name",
+                  ),
+                  _buildSingleContainer(
+                    endText: "${loggedInUser.secondName}",
+                    startText: "Second Name",
+                  ),
+                  _buildSingleContainer(
+                    endText: "${loggedInUser.email}",
+                    startText: "Email",
+                  ),
+                  _buildSingleContainer(
+                    endText: "0192121",
+                    startText: "Phone Number",
+                  ),
+                  _buildSingleContainer(
+                    endText: "A19121",
+                    startText: "Matric Card",
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      editButton,
+      ActionChip(
+          label: Text("Logout"),
+          onPressed: () {
+            logout(context);
+          }),
+    ];
     return Scaffold(
       backgroundColor: Color(0xfff8f8f8),
       appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: Colors.white,
-        leading:
-          IconButton(icon: Icon(Icons.arrow_back_rounded, color: Colors.black,), onPressed: (){
-            Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-          },)
-      ),
+          title: const Text('Profile'),
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+          )),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 140,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    
-                    children: [
-                      // Image.asset("assets/logo.png", fit: BoxFit.contain),
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        maxRadius: 70,
-                        backgroundImage: AssetImage("assets/eleceed.jpg"),
-                      ),
-                     ],
-                  ),
-                ),
-              ],
-            ),
-          Container(
-                    height: 300,
-                    // color: Colors.blue,
-                    width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          height: 300,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              _buildSingleContainer(
-                                endText: "${loggedInUser.firstName}",
-                                startText: "First Name",
-                              ),
-                              _buildSingleContainer(
-                                endText: "${loggedInUser.secondName}",
-                                startText: "Second Name",
-                              ),
-                              _buildSingleContainer(
-                                endText: "${loggedInUser.email}",
-                                startText: "Email",
-                              ),
-                              _buildSingleContainer(
-                                endText: "0192121",
-                                startText: "Phone Number",
-                              ),
-                              _buildSingleContainer(
-                                endText: "A19121",
-                                startText: "Matric Card",
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-            ),
-            editButton,
-          ],
+          children: children2,
         ),
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 }
