b6635b959dbb0d380b0d2f6b2203385c880ddc27
farisfaisal01 <farisfaisal100@gmail.com>
2022-06-26 01:26:38 +0800

    local noti

---
+++ bundle_plus/lib/screens/noti_api.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotiApi {
+  static final _noti = FlutterLocalNotificationsPlugin();
+
+  static Future _notificationDetails() async {
+    // ignore: prefer_const_constructors
+    return NotificationDetails(
+      // ignore: prefer_const_constructors
+      android: AndroidNotificationDetails(
+        'channel id',
+        'channel name',
+        'channel description',
+        importance: Importance.max,
+      ),
+    );
+  }
+
+  static Future showNoti({
+    int id = 0,
+    String? title,
+    String? body,
+    String? payload,
+  }) async =>
+      _noti.show(
+        id,
+        title,
+        body,
+        await _notificationDetails(),
+        payload: payload,
+      );
+}
